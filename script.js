955113259fa0d8c6c7ef81a210c3f9ec3e7e6946
Valentin <52529145+YunaBlood@users.noreply.github.com>
2024-10-16 07:41:23 +0200

    close #10 Add addition function

--- script.js
+++ script.js
@@ -13,3 +13,30 @@
 12.Add keyboard support
 13.End program
 */
+
+
+//1.Calculator function
+
+//Addition calculation
+
+//Define the function addition with three paremeters two number and a operator
+function addition(firstNumber, operator, secondNumber) {
+
+//Convert input to numbers first
+let num1 = Number(firstNumber);
+let num2 = Number(secondNumber);
+
+//If number is not a number 
+if(Number.isNaN(num1) || Number.isNaN(num2)){
+    //Display error
+    alert(`Invalid data type: \nthe first Number is ${typeof(firstNumber)} \nThe second number is ${typeof(secondNumber)} \nboth value should be a number`)
+    //Return null
+    return null
+}
+//Perform the addition
+let resultNumber = num1 + num2;
+//Return the result 
+return resultNumber;
+
+}
+console.log(addition("1", "", 1))
