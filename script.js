79dec3ac77b7767fffb7d8d820597950c18d496f
Valentin <52529145+YunaBlood@users.noreply.github.com>
2024-10-16 06:52:32 +0200

    Added high level pseudocode

---
+++ script.js
@@ -0,0 +1,15 @@
+/* High level pseudo code:
+1.Calculator function with all operator
+2.Get user input
+3.Validate user input
+4.Display user input 
+5.Store the user input and the operator
+6. Call the appropriate operator function (addition, subtraction, multiplication, etc.) when "=" is pressed
+7.Display result
+8.Ensure the calculator evaluate only one pair of number a times
+9.Handle error and edge cases
+10.Division by 0 or multiple decimal points
+11.Handle "=" key when clicked and not all number are on the display
+12.Add keyboard support
+13.End program
+*/
